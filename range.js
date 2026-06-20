df580708e3572bf80b39f65cc7d668b4fa41ae95
SumiAkter2 <akteralisha36@gmail.com>
2026-06-20 16:33:06 +0600

    learn how to find sum and even or odd number

--- range.js
+++ range.js
@@ -5,3 +5,27 @@ while (num < 40) {
   console.log(num + 1);
   num += 1;
 }
+
+// sum ber kora:
+
+var number = 5;
+var sum = 0;
+
+while (number <= 20) {
+  console.log(number);
+  sum = sum + number;
+  console.log("sum:", sum);
+  number++;
+}
+
+// ever number ber kora:
+
+var rollNumber = 1;
+
+while (rollNumber <= 20) {
+  console.log(rollNumber);
+  if (rollNumber % 2 !== 0) {
+    console.log("odd number is:", rollNumber);
+  }
+  rollNumber++;
+}
