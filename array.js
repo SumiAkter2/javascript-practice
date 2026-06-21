bdd4861b75ff021ee36836abd8be6284bebcf806
SumiAkter2 <akteralisha36@gmail.com>
2026-06-21 16:25:20 +0600

    added array

---
+++ array.js
@@ -0,0 +1,15 @@
+const carName = ["BNJ", "TMD", "Xys", 5, true];
+console.log(carName);
+
+const car = new Array("ss", "gg", "dd");
+console.log(car);
+
+console.log(carName[2]);
+// console.log(Object.keys(carName));
+console.log(Object.values(carName));
+
+console.log(carName.length);
+
+carName[4] = "Toyota";
+carName.push("Honda");
+console.log(carName)
